8aad9a115bdb0c8e8648bf6a70c367f9993b0da3
Agnieszka Klekot <74aklekot@gmail.com>
2024-10-30 11:01:42 +0100

    Redux

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,23 +1,29 @@
+import { useSelector } from 'react-redux';
 import styles from './Column.module.scss';
 import Card from './../Card/Card';
 import CardForm from './../CardForm/CardForm';
 
-const Column = ({ id, title, icon, cards, addCard }) => {
-    const handleAddCard = (newCard) => {
-        addCard(newCard, id);
-    };
-    return <article className={styles.column}>
+const Column = ({ id, title, icon }) => {
+  // Select cards for the current column by filtering from the Redux store
+  const cards = useSelector(state => 
+    state.cards.filter(card => card.columnId === id)
+  );
+
+  return (
+    <div className={styles.column}>
+      <header className={styles.header}>
         <h2 className={styles.title}>
-            <span className={styles.icon + ' fa fa-' + icon} />
-            {title}
+          {title} <span>{icon}</span>
         </h2>
-        <ul className={styles.cards}>
-                {cards.map(card => (
-                    <Card key={card.id} title={card.title} />
-                ))}
-            </ul>
-            <CardForm action={handleAddCard} />
-    </article>
-}
+      </header>
+      <section className={styles.cards}>
+        {cards.map(card => (
+          <Card key={card.id} {...card} />
+        ))}
+      </section>
+      <CardForm columnId={id} />
+    </div>
+  );
+};
 
 export default Column;
